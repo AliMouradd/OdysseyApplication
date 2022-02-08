b6d8b4816e27c6e34c57178c699ef15d834546b7
Danny Alvarez <48600515+SolarMango@users.noreply.github.com>
2022-02-08 15:11:54 -0800

    added questionnaire start screen

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import ExampleScreen from "./screens/ExampleScreen";
+import QuestionnaireStartScreen from "./screens/QuestionnaireStartScreen";
 
 const Stack = createNativeStackNavigator();
 
---
+++ screens/QuestionnaireStartScreen.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { View, Button } from "react-native";
+
+export default function QuestionnaireStartScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to the Odyssey Questionnaire!</Text>
+      <Text>
+        Here we will ask you a series of questions about your trip in order to
+        help you with your vacation.
+      </Text>
+      <Button
+        title="Next"
+        onPress={console.log("Next button pressed")}
+      ></Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  nextButton: {},
+});
