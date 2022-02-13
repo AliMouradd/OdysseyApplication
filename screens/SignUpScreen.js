54db5543e1344c629798a38cea1b53c344426cea
Nicholas Chan <74500036+n-chan@users.noreply.github.com>
2022-02-12 19:03:13 -0800

    Edited sign up screen. Added sign up functionality
    Minor design change to sign up screen. New user accounts can now be added to database.

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -16,6 +16,7 @@ import {
   Text,
   TouchableOpacity,
 } from "react-native";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 const SignUpScreen = () => {
   const [fullName, setfullName] = useState("");
@@ -23,28 +24,38 @@ const SignUpScreen = () => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
+  const auth = getAuth();
+
   const signup = () => {
-    //todo
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredentials) => {
+        const user = userCredentials.user;
+      })
+      .catch((error) => alert(error.essage));
   };
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Create Account with Email</Text>
+      <Text style={styles.h1}>Create Account with Email</Text>
+      <Text style={styles.label}>Full Name:</Text>
       <TextInput
         style={styles.input}
         onChangeText={(value) => setfullName(value)}
         value={fullName}
       />
+      <Text style={styles.label}>Email:</Text>
       <TextInput
         style={styles.input}
         onChangeText={(value) => setEmail(value)}
         value={email}
       />
+      <Text style={styles.label}>Password:</Text>
       <TextInput
         style={styles.input}
         onChangeText={(value) => setPassword(value)}
         value={password}
       />
+      <Text style={styles.label}>Confirm Password:</Text>
       <TextInput
         style={styles.input}
         onChangeText={(value) => setConfirmPassword(value)}
@@ -59,8 +70,15 @@ const SignUpScreen = () => {
 
 const styles = StyleSheet.create({
   container: {
-    width: "100%",
+    flex: 1,
     alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+  },
+  h1: {
+    color: "black",
+    fontSize: 24,
+    marginBottom: 10,
   },
   logo: {
     fontSize: 20,
