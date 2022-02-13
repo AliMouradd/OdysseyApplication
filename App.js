b176490a0db868011190cddae5bdd83b3adafab7
Danny Alvarez <danalvarezsol@gmail.com>
2022-02-12 22:01:33 -0800

    added very simple homepage UI

--- App.js
+++ App.js
@@ -2,14 +2,15 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import ExampleScreen from "./screens/ExampleScreen";
+import HomepageScreen from "./screens/HomepageScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer initialRouteName="Example">
+    <NavigationContainer initialRouteName="Homepage">
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Example" component={ExampleScreen} />
+        <Stack.Screen name="Homepage" component={HomepageScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ screens/HomepageScreen.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+  Button,
+} from "react-native";
+import { getAuth } from "firebase/auth";
+
+const HomepageScreen = ({ navigation, route }) => {
+  console.log("email: " + route.params.email);
+
+  return (
+    <View>
+      <Text>Welcome to Odyssey!</Text>
+      <Text>{route.params.email}</Text>
+      <Button title="Explore"></Button>
+      <Button title="Create"></Button>
+      <Button title="Questionnaire"></Button>
+      <Button title="Schedules"></Button>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default HomepageScreen;
