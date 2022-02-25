d2b832afe11d259ffd677097df1d3bae3a3cea0b
Ali Mourad <ali.mourad@student.csulb.edu>
2022-02-24 16:31:53 -0800

    HomeScreen Design Implementation

--- screens/HomepageScreen.js
+++ screens/HomepageScreen.js
@@ -6,6 +6,7 @@ import {
   TouchableOpacity,
   View,
   Button,
+  Image
 } from "react-native";
 import { getAuth } from "firebase/auth";
 
@@ -13,17 +14,64 @@ const HomepageScreen = ({ navigation, route }) => {
   //console.log("email: " + route.params.email);
 
   return (
-    <View>
-      <Text>Welcome to Odyssey!</Text>
+    <View style = {styles.container}>
+
+    <Image 
+      style = {styles.image}
+      source = {require("../assets/Flyingkite_Flatline.png")}
+      
+      />
+    
       {/*<Text>{route.params.email}</Text>*/}
-      <Button title="Explore"></Button>
-      <Button title="Create"></Button>
-      <Button title="Questionnaire"></Button>
-      <Button title="Schedules"></Button>
+      <View style = {styles.buttonsContainer}>
+      <TouchableOpacity style={styles.btn}>
+        <Text>Explore</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.btn}>
+        <Text>Create</Text>
+      </TouchableOpacity>
+      </View>
+
+      <View style = {styles.buttonsContainer}>
+      <TouchableOpacity style={styles.btn}>
+        <Text>Questionaire</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.btn}>
+        <Text>Schedules</Text>
+      </TouchableOpacity>
+      </View>
+      
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    alignItems: "center",
+    textAlign:"center",
+    marginTop: 75,
+    flex: 1
+  },
+  btn: {
+    borderRadius: 10,
+    width: "30%",
+    margin: 5,
+    backgroundColor: "#FFD56D",
+    padding: 10,
+    textAlign: "center",
+    alignItems: "center"
+  },
+  buttonsContainer:{
+    flexDirection: "row"
+   
+  },
+
+  image:{
+    height: 250,
+    width: 250,
+    marginBottom: 50
+  }
+});
 
 export default HomepageScreen;
