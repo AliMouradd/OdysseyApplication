4e9c78aa8788679ddb2b85d4ff7fc19343439190
Aim <47038179+AliMouradd@users.noreply.github.com>
2022-02-26 00:46:10 -0800

    Preliminary example of Itinerary Screen
    Work in progress...

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import ExampleScreen from "./screens/ExampleScreen";
 import WelcomeScreen from "./screens/WelcomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import SignUpScreen from "./screens/SignUpScreen";
+import HomeScreen from "./screens/HomeScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,10 +13,10 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Welcome"
+        initialRouteName="Example"
         screenOptions={{ headerShown: true }}
       >
-        <Stack.Screen name="Example" component={ExampleScreen} />
+        <Stack.Screen name="Example" component={HomeScreen} />
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
         <Stack.Screen name="Log In" component={LoginScreen} />
         <Stack.Screen name="Sign Up" component={SignUpScreen} />
