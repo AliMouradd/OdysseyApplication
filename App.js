a19045b187e5a7412e1c62acebf3eeed1784dd47
Ali Mourad <ali.mourad@student.csulb.edu>
2022-03-01 16:23:15 -0800

    Updated App.js
    Updated app.js to include homepage as well as delete ExampleScreen

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import ExampleScreen from "./screens/ExampleScreen";
 import WelcomeScreen from "./screens/WelcomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import SignUpScreen from "./screens/SignUpScreen";
+import HomepageScreen from "./screens/HomepageScreen";
+
 
 const Stack = createNativeStackNavigator();
 
@@ -15,7 +16,7 @@ export default function App() {
         initialRouteName="Welcome"
         screenOptions={{ headerShown: true }}
       >
-        <Stack.Screen name="Example" component={ExampleScreen} />
+        <Stack.Screen name="Home" component={HomepageScreen} />
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
         <Stack.Screen name="Log In" component={LoginScreen} />
         <Stack.Screen name="Sign Up" component={SignUpScreen} />
