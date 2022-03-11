00cb072f6803ffa0089890ac2b0ba395f220940d
jacobsamson <jacob.samson@student.csulb.edu>
2022-03-10 16:17:31 -0800

    Login 3 and Sign up 3 Merged

--- App.js
+++ App.js
@@ -5,28 +5,25 @@ import WelcomeScreen from "./screens/WelcomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import SignUpScreen from "./screens/SignUpScreen";
 import HomepageScreen from "./screens/HomepageScreen";
-
-
 import ProfileScreen from "./screens/ProfileScreen";
 import ItineraryScreen from "./screens/ItineraryScreen";
 
 const Stack = createNativeStackNavigator();
 
+
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Home"
+        initialRouteName="Log In"
         screenOptions={{ headerShown: true }}
       >
+        <Stack.Screen name="Sign Up" component={SignUpScreen} />
         <Stack.Screen name="Home" component={HomepageScreen} />
         <Stack.Screen name="Itinerary" component={ItineraryScreen} />
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
         <Stack.Screen name="Log In" component={LoginScreen} />
-        <Stack.Screen name="Sign Up" component={SignUpScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-//testing -jacob
