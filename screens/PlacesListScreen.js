6331ee3a60c1f91d52d08bea4c6dd376f08f5d49
Nicholas Chan <74500036+n-chan@users.noreply.github.com>
2022-03-19 17:25:00 -0700

    Edited places list screen
    Implemented sorting and filtering feature. Created basic modal for sorting and filtering. Created basic modal for editing text.

--- screens/PlacesListScreen.js
+++ screens/PlacesListScreen.js
@@ -6,60 +6,113 @@ import {
   TouchableOpacity,
   ScrollView,
   View,
+  Modal,
+  TouchableWithoutFeedback,
+  TextInput,
 } from "react-native";
 import PlacesComponent from "./PlacesComponent";
 import Icon from "react-native-vector-icons/MaterialIcons";
 
 const PlacesListScreen = ({ navigation }) => {
+  const [scheduleName, setScheduleName] = useState("Name of the Schedule");
+  const [description, setDescription] = useState(
+    "Short Description of the Trip"
+  );
   const [places, setPlaces] = useState([]);
+  const [sortModalVisible, setSortModalVisible] = useState(false);
+  const [filterModalVisible, setFilterModalVisible] = useState(false);
+  const [nameInputModalVisible, setNameInputModalVisible] = useState(false);
+  const [descriptionModalVisible, setDescriptionModalVisible] = useState(false);
 
   useEffect(() => {
     getPlacesTest();
   }, []);
 
+  const sortPlacesAsc = () => {
+    const sortedPlaces = [...places].sort(function (a, b) {
+      if (a.name.toLowerCase() < b.name.toLowerCase()) {
+        return -1;
+      }
+      if (a.name.toLowerCase() > b.name.toLowerCase()) {
+        return 1;
+      }
+      return 0;
+    });
+    setPlaces(sortedPlaces);
+  };
+
+  const sortPlacesDes = () => {
+    const sortedPlaces = [...places].sort(function (a, b) {
+      if (a.name.toLowerCase() < b.name.toLowerCase()) {
+        return 1;
+      }
+      if (a.name.toLowerCase() > b.name.toLowerCase()) {
+        return -1;
+      }
+      return 0;
+    });
+    setPlaces(sortedPlaces);
+  };
+
+  const filterPlaces = (t) => {
+    getPlacesTest();
+    if (t === "All") {
+      return;
+    }
+    const filteredPlaces = [...places].filter((p) => p.type === t);
+    setPlaces(filteredPlaces);
+  };
+
   const getPlacesTest = () => {
     let sampleData = [
       {
         number: 1,
-        name: "Place One",
+        name: "Z",
         description:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+        type: "Restaurant",
       },
       {
         number: 2,
-        name: "Place Two",
+        name: "A",
         description:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+        type: "Restaurant",
       },
       {
         number: 3,
-        name: "Place Three",
+        name: "C",
         description:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+        type: "Attraction",
       },
       {
         number: 4,
-        name: "Place Four",
+        name: "B",
         description:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+        type: "Store",
       },
       {
         number: 5,
-        name: "Place Five",
+        name: "G",
         description:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+        type: "Park",
       },
       {
         number: 6,
-        name: "Place Six",
+        name: "H",
         description:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+        type: "Store",
       },
       {
         number: 7,
-        name: "Place Seven",
+        name: "I",
         description:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+        type: "Park",
       },
     ];
     setPlaces(sampleData);
@@ -76,14 +129,20 @@ const PlacesListScreen = ({ navigation }) => {
           </View>
           <View style={styles.infoWrapper}>
             <View style={styles.info}>
-              <Text style={styles.h1}>Name Of the Schedule</Text>
-              <TouchableOpacity>
+              <Text style={styles.h1}>{scheduleName}</Text>
+              <TouchableOpacity
+                onPress={() => setNameInputModalVisible(!nameInputModalVisible)}
+              >
                 <Icon name="edit" size={20} color="black" />
               </TouchableOpacity>
             </View>
             <View style={styles.info}>
-              <Text style={styles.p}>Short Description of the Trip</Text>
-              <TouchableOpacity>
+              <Text style={styles.p}>{description}</Text>
+              <TouchableOpacity
+                onPress={() =>
+                  setDescriptionModalVisible(!descriptionModalVisible)
+                }
+              >
                 <Icon name="edit" size={20} color="black" />
               </TouchableOpacity>
             </View>
@@ -103,15 +162,22 @@ const PlacesListScreen = ({ navigation }) => {
           <TouchableOpacity>
             <Icon name="edit" size={20} color="black" />
           </TouchableOpacity>
-          <TouchableOpacity>
-            <Text>Sort/Filter</Text>
+          <TouchableOpacity
+            onPress={() => setSortModalVisible(!sortModalVisible)}
+          >
+            <Text>Sort</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => setFilterModalVisible(!filterModalVisible)}
+          >
+            <Text>Filter</Text>
           </TouchableOpacity>
         </View>
 
         <View style={styles.wrapper}>
           {places.map((place) => (
             <PlacesComponent
-              key={place.name}
+              key={place.number}
               place={place}
               navigation={navigation}
             />
@@ -123,6 +189,143 @@ const PlacesListScreen = ({ navigation }) => {
           <Text style={{ fontWeight: "bold", fontSize: 16 }}>Generate</Text>
         </TouchableOpacity>
       </View>
+
+      <Modal
+        transparent={true}
+        visible={nameInputModalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setNameInputModalVisible(!nameInputModalVisible);
+        }}
+      >
+        <TouchableWithoutFeedback
+          onPress={() => setNameInputModalVisible(!nameInputModalVisible)}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <TextInput value={scheduleName} onChangeText={setScheduleName} />
+              <View>
+                <TouchableOpacity
+                  style={styles.btn}
+                  onPress={() =>
+                    setNameInputModalVisible(!nameInputModalVisible)
+                  }
+                >
+                  <Text style={{ fontWeight: "bold", fontSize: 16 }}>Okay</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.btn}
+                  onPress={() =>
+                    setNameInputModalVisible(!nameInputModalVisible)
+                  }
+                >
+                  <Text style={{ fontWeight: "bold", fontSize: 16 }}>
+                    Cancel
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </Modal>
+
+      <Modal
+        transparent={true}
+        visible={descriptionModalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setDescriptionModalVisible(!descriptionModalVisible);
+        }}
+      >
+        <TouchableWithoutFeedback
+          onPress={() => setDescriptionModalVisible(!descriptionModalVisible)}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <TextInput value={description} onChangeText={setDescription} />
+              <View>
+                <TouchableOpacity
+                  style={styles.btn}
+                  onPress={() =>
+                    setDescriptionModalVisible(!descriptionModalVisible)
+                  }
+                >
+                  <Text style={{ fontWeight: "bold", fontSize: 16 }}>Okay</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.btn}
+                  onPress={() =>
+                    setDescriptionModalVisible(!descriptionModalVisible)
+                  }
+                >
+                  <Text style={{ fontWeight: "bold", fontSize: 16 }}>
+                    Cancel
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </Modal>
+
+      <Modal
+        transparent={true}
+        visible={sortModalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setSortModalVisible(!sortModalVisible);
+        }}
+      >
+        <TouchableWithoutFeedback
+          onPress={() => setSortModalVisible(!sortModalVisible)}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalTitle}>Sort By</Text>
+              <TouchableOpacity onPress={() => sortPlacesAsc()}>
+                <Text style={styles.modalText}>A to Z</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => sortPlacesDes()}>
+                <Text style={styles.modalText}>Z to A</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </Modal>
+
+      <Modal
+        transparent={true}
+        visible={filterModalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setFilterModalVisible(!filterModalVisible);
+        }}
+      >
+        <TouchableWithoutFeedback
+          onPress={() => setFilterModalVisible(!filterModalVisible)}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalTitle}>Filter By</Text>
+              <TouchableOpacity onPress={() => filterPlaces("All")}>
+                <Text style={styles.modalText}>All</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => filterPlaces("Park")}>
+                <Text style={styles.modalText}>Park</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => filterPlaces("Restaurant")}>
+                <Text style={styles.modalText}>Restaurant</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => filterPlaces("Store")}>
+                <Text style={styles.modalText}>Store</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => filterPlaces("Attraction")}>
+                <Text style={styles.modalText}>Attraction</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </Modal>
     </SafeAreaView>
   );
 };
@@ -162,6 +365,30 @@ const styles = StyleSheet.create({
     padding: 10,
     paddingLeft: 50,
     paddingRight: 50,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+  modalView: {
+    alignItems: "center",
+    backgroundColor: "white",
+    elevation: 5,
+    paddingLeft: 25,
+    paddingRight: 25,
+    paddingTop: 5,
+    paddingBottom: 5,
+  },
+  modalTitle: {
+    marginBottom: 5,
+    fontSize: 18,
+    borderBottomWidth: 2,
+    borderColor: "black",
+  },
+  modalText: {
+    fontSize: 16,
   },
 });
 
