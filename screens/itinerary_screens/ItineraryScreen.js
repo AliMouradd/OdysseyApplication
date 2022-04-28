d3d3f0c159a6138518d5cf980b49e160abff567b
Danny Alvarez <danalvarezsol@gmail.com>
2022-04-28 01:17:24 -0700

    Updated adding events + other fixes
    Added selecting from list of places when adding event, added moment dates here and there, fixed calendar strip re-rendering weirdness.

--- screens/itinerary_screens/ItineraryScreen.js
+++ screens/itinerary_screens/ItineraryScreen.js
@@ -6,8 +6,8 @@ import {
   Text,
   FlatList,
   Modal,
-  Pressable,
   TextInput,
+  ToastAndroid,
 } from "react-native";
 import CalendarStrip from "react-native-calendar-strip";
 import Icon from "react-native-vector-icons/MaterialIcons";
@@ -30,12 +30,12 @@ const ItineraryScreen = ({ navigation }) => {
   const [selectedDate, setSelectedDate] = useState(undefined);
   const [formattedDate, setFormattedDate] = useState();
   const [startDate, setStartDate] = useState(
-    moment("04/20/2023").format("MM/DD/YYYY")
+    moment(new Date()).format("MM/DD/YYYY")
   ); // change this to make the calendar strip start at correct date
-  const [tripStartDate, setTripStartDate] = useState();
-  const [tripEndDate, setTripEndDate] = useState();
-  const [events, setEvents] = useState([]); // array of event maps for all dates (should include date, time, and description)
-  const [selectedEvents, setSelectedEvents] = useState([]); // array of event maps for selected date
+  const [tripStartDate, setTripStartDate] = useState(undefined);
+  const [tripEndDate, setTripEndDate] = useState(undefined);
+  const [events, setEvents] = useState([]); // array of event objects for all dates (should include date, time, and description)
+  const [selectedEvents, setSelectedEvents] = useState([]); // array of event objects for selected date
   const [markedDates, setMarkedDates] = useState([]);
   const [modalVisible, setModalVisible] = useState(false);
 
@@ -43,6 +43,7 @@ const ItineraryScreen = ({ navigation }) => {
   const [newTitle, setNewTitle] = useState("");
   const [newDate, setNewDate] = useState("");
   const [newTime, setNewTime] = useState("");
+  const [selectedPlace, setSelectedPlace] = useState("");
 
   // state for places list
   const [places, setPlaces] = useState([]);
@@ -58,6 +59,7 @@ const ItineraryScreen = ({ navigation }) => {
   const placesListDocRef = doc(db, "UserSchedules", "123");
 
   // get all user's events from database (might run after each rerender?)
+  // maybe get all data from a previous screen and pass it into this screen to avoid rendering issue
   useEffect(() => {
     getTripStartEndDates(); // get trip start/end dates from survey (try getting these before calendar strip renders somehow)
     createSchedDoc(); // create user's doc if not already created
@@ -65,7 +67,24 @@ const ItineraryScreen = ({ navigation }) => {
     getFromPlacesList();
   }, []);
 
+  // function to get vacation start and end dates (to pass to calendar strip)
+  const getTripStartEndDates = () => {
+    console.log("getTripStartEndDates called!");
+    getDoc(userSurveyDocRef).then((doc) => {
+      console.log("Getting trip start and end dates...");
+      setTripStartDate(moment(doc.get("startDate")).format("MM/DD/YYYY"));
+      setTripEndDate(moment(doc.get("endDate")).format("MM/DD/YYYY"));
+      setStartDate(moment(doc.get("startDate")).format("MM/DD/YYYY"));
+
+      // for testing, show uid and start/end dates
+      // console.log(uid);
+      // console.log(doc.get("startDate"));
+      // console.log(doc.get("endDate"));
+    });
+  };
+
   const createSchedDoc = () => {
+    console.log("createSchedDoc called!");
     setDoc(userSchedDocRef, {}, { merge: true })
       .then(() => {
         //alert("Document Created/Updated");
@@ -75,83 +94,73 @@ const ItineraryScreen = ({ navigation }) => {
       });
   };
 
+  // function to get events for date selected from database
+  const getEventsFromDatabase = () => {
+    console.log("getEventsFromDatabase called!");
+    // function should take date as parameter
+    getDoc(userSchedDocRef).then((doc) => {
+      console.log("Getting events...");
+      setEvents(doc.get("events")); // gets the array of event objects from database
+      //console.log(events);
+    });
+  };
+
+  // function to add events to database
   const addEventToDatabase = () => {
+    console.log("addEventToDatabase called!");
     const data = {
       title: newTitle,
       time: newTime,
-      date: newDate,
+      date: moment(newDate).format("MM/DD/YYYY"),
+      place: selectedPlace,
       id: Math.random(),
     };
     const newEventsArray = [...events, data];
     updateDoc(userSchedDocRef, { events: newEventsArray }, { merge: true })
       .then(() => {
-        alert("Event added");
+        ToastAndroid.show("Event added", ToastAndroid.SHORT);
       })
       .catch((error) => {
         alert(error.message);
       });
   };
 
-  // function to get events for date selected from database
-  const getEventsFromDatabase = () => {
-    // function should take date as parameter
-    getDoc(userSchedDocRef).then((doc) => {
-      console.log("Getting events...");
-      setEvents(doc.get("events")); // gets the array of maps from database
-      console.log(events);
-    });
-  };
-
-  // function to get vacation start and end dates (to pass to calendar strip)
-  const getTripStartEndDates = () => {
-    getDoc(userSurveyDocRef).then((doc) => {
-      console.log("Getting trip start and end dates...");
-      setTripStartDate(doc.get("startDate"));
-      setTripEndDate(doc.get("endDate"));
-      setStartDate(doc.get("startDate"));
-
-      // for testing, show uid and start/end dates
-      console.log(uid);
-      console.log(doc.get("startDate"));
-      console.log(doc.get("endDate"));
-    });
-  };
-
   const getFromPlacesList = () => {
+    console.log("getFromPlacesList called!");
     getDoc(placesListDocRef).then((doc) => {
       setPlaces(doc.get("places"));
     });
-    console.log(places);
+    //console.log(places);
+  };
+
+  // function called when date is selected from calendar strip
+  const onDateSelected = (date) => {
+    console.log("onDateSelected called!");
+    //console.log("\ndate variable: ", date);
+    //console.log("old selectedDate state: ", selectedDate);
+    const formDate = date.format("MM/DD/YYYY");
+    setSelectedDate(formDate);
+    getEventsForDay(formDate);
   };
 
   const getEventsForDay = (formDate) => {
-    // WONT WORK, GETS OLD selectedDate STATE, FIGURE OUT HOW TO GET MOST RECENT STATE
+    console.log("getEventsForDay called!");
     if (events == undefined) {
       alert("No events created at all!"); // user hasn't added any events yet
     } else {
       const eventObjectsForDay = events.filter(
         (event) => event.date === formDate
       );
-      console.log("selectedDate state: ", selectedDate);
-      console.log("\nEvent Objects for selected day:\n", eventObjectsForDay);
+      //console.log("selectedDate state: ", selectedDate);
+      //console.log("\nEvent Objects for selected day:\n", eventObjectsForDay);
       setSelectedEvents(eventObjectsForDay);
     }
-  };
-
-  // function called when date is selected from calendar strip
-  const onDateSelected = (date) => {
-    console.log("\ndate variable: ", date);
-    console.log("old selectedDate state: ", selectedDate);
-    const formDate = date.format("MM/DD/YYYY");
-    setSelectedDate(formDate);
-    getEventsForDay(formDate);
-    //console.log("new selectedDate state: ", selectedDate); this wont work because state is async, wont update immediately
   };
 
   // List Item component, renders each event in the agenda list
   const ListItem = ({ event }) => {
     // each event is one object from the events array
-    console.log(event); // shows each event object
+    //console.log(event); // shows each event object
 
     return (
       <View>
@@ -159,16 +168,23 @@ const ItineraryScreen = ({ navigation }) => {
           <Text>title: {event.item.title}</Text>
           <Text>date: {event.item.date}</Text>
           <Text>time: {event.item.time}</Text>
+          <Text>place: {event.item.place}</Text>
         </TouchableOpacity>
       </View>
     );
   };
 
   const PlacesListItem = ({ place }) => {
-    const [selectedPlace, setSelectedPlace] = useState();
     return (
       <View>
-        <TouchableOpacity style={styles.placecontainer}>
+        <TouchableOpacity
+          style={
+            place.item.name == selectedPlace
+              ? styles.selectedplacecontainer
+              : styles.unselectedplacecontainer
+          }
+          onPress={() => setSelectedPlace(place.item.name)}
+        >
           <Text>{place.item.name}</Text>
           <Text>{place.item.type}</Text>
         </TouchableOpacity>
@@ -177,107 +193,120 @@ const ItineraryScreen = ({ navigation }) => {
   };
 
   // main return
-  return (
-    <View style={styles.maincontainer}>
-      <CalendarStrip
-        scrollable={false}
-        calendarAnimation={{ type: "parallel", duration: 600 }}
-        daySelectionAnimation={{
-          type: "background",
-          duration: 300,
-          highlightColor: "#9265DC",
-        }}
-        style={{ height: 120, paddingTop: 20, paddingBottom: 10 }}
-        calendarHeaderStyle={{ color: "black" }}
-        calendarColor={"#FFD56D"}
-        dateNumberStyle={{ color: "black" }}
-        dateNameStyle={{ color: "black" }}
-        iconContainer={{ flex: 0.1 }}
-        highlightDateNameStyle={{ color: "white" }}
-        highlightDateNumberStyle={{ color: "white" }}
-        highlightDateContainerStyle={{ backgroundColor: "black" }}
-        markedDates={markedDates}
-        selectedDate={selectedDate}
-        onDateSelected={onDateSelected}
-        useIsoWeekday={false}
-        startingDate={tripStartDate}
-        minDate={tripStartDate}
-        maxDate={tripEndDate}
-      />
-
-      <Text style={{ fontSize: 24 }}>Selected Date: {selectedDate}</Text>
-
+  if (tripStartDate == undefined) {
+    return (
       <View>
-        <FlatList
-          data={selectedEvents}
-          renderItem={(item) => <ListItem event={item} />}
-        />
+        <Text>Loading...</Text>
       </View>
+    );
+  } else {
+    return (
+      <View style={styles.maincontainer}>
+        <CalendarStrip
+          scrollable
+          calendarAnimation={{ type: "parallel", duration: 600 }}
+          daySelectionAnimation={{
+            type: "background",
+            duration: 300,
+            highlightColor: "#9265DC",
+          }}
+          style={{ height: 120, paddingTop: 20, paddingBottom: 10 }}
+          calendarHeaderStyle={{ color: "black" }}
+          calendarColor={"#FFD56D"}
+          dateNumberStyle={{ color: "black" }}
+          dateNameStyle={{ color: "black" }}
+          iconContainer={{ flex: 0.1 }}
+          highlightDateNameStyle={{ color: "white" }}
+          highlightDateNumberStyle={{ color: "white" }}
+          highlightDateContainerStyle={{ backgroundColor: "black" }}
+          markedDates={markedDates}
+          selectedDate={selectedDate}
+          onDateSelected={onDateSelected}
+          useIsoWeekday={false}
+          startingDate={tripStartDate}
+          minDate={tripStartDate}
+          maxDate={tripEndDate}
+        />
 
-      <TouchableOpacity
-        style={styles.addeventbutton}
-        onPress={() => setModalVisible(true)}
-      >
-        <Icon name="add" size={33} color="black" />
-      </TouchableOpacity>
+        <Text style={{ fontSize: 24 }}>Selected Date: {selectedDate}</Text>
 
-      <Modal
-        animationType="fade"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          Alert.alert("Modal has been closed.");
-          setModalVisible(!modalVisible);
-        }}
-      >
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <Text style={{ fontSize: 19, fontWeight: "bold" }}>
-              Enter new event details:
-            </Text>
-            <TextInput
-              style={styles.textbox}
-              onChangeText={setNewTitle}
-              value={newTitle}
-              placeholder="Enter title..."
-            />
-            <TextInput
-              style={styles.textbox}
-              onChangeText={setNewDate}
-              value={newDate}
-              placeholder="Enter date (MM/DD/YYYY)"
-            />
-            <TextInput
-              style={styles.textbox}
-              onChangeText={setNewTime}
-              value={newTime}
-              placeholder="Enter time (HH:MM:SS AM/PM)"
-            />
-            <View style={{ height: 300 }}>
-              <FlatList
-                data={places}
-                renderItem={(item) => <PlacesListItem place={item} />}
+        <View>
+          <FlatList
+            data={selectedEvents}
+            renderItem={(item) => <ListItem event={item} />}
+          />
+        </View>
+
+        <TouchableOpacity
+          style={styles.addeventbutton}
+          onPress={() => [setModalVisible(true), setSelectedPlace("")]}
+        >
+          <Icon name="add" size={33} color="black" />
+        </TouchableOpacity>
+
+        <Modal
+          animationType="fade"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            setModalVisible(!modalVisible);
+          }}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={{ fontSize: 19, fontWeight: "bold" }}>
+                Enter new event details:
+              </Text>
+              <TextInput
+                style={styles.textbox}
+                onChangeText={setNewTitle}
+                value={newTitle}
+                placeholder="Enter title..."
               />
-            </View>
-            <View style={styles.modalbuttonsview}>
-              <TouchableOpacity
-                style={styles.modalbutton}
-                onPress={() => setModalVisible(false)}
-              >
-                <Text>Cancel</Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={styles.modalbutton}
-                onPress={() => [addEventToDatabase(), setModalVisible(false)]}
-              >
-                <Text>Save</Text>
-              </TouchableOpacity>
+              <TextInput
+                style={styles.textbox}
+                onChangeText={setNewDate}
+                value={newDate}
+                placeholder="Enter date (MM/DD/YYYY)"
+              />
+              <TextInput
+                style={styles.textbox}
+                onChangeText={setNewTime}
+                value={newTime}
+                placeholder="Enter time (HH:MM:SS AM/PM)"
+              />
+              <View style={{ height: "55%", margin: 10 }}>
+                <Text>Choose a place from your list:</Text>
+                <Text>Selected place: {selectedPlace}</Text>
+                <FlatList
+                  data={places}
+                  renderItem={(item) => <PlacesListItem place={item} />}
+                />
+              </View>
+              <View style={styles.modalbuttonsview}>
+                <TouchableOpacity
+                  style={styles.modalbutton}
+                  onPress={() => setModalVisible(false)}
+                >
+                  <Text>Cancel</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.modalbutton}
+                  onPress={() => [
+                    addEventToDatabase(),
+                    getEventsFromDatabase(),
+                    setModalVisible(false),
+                  ]}
+                >
+                  <Text>Save</Text>
+                </TouchableOpacity>
+              </View>
             </View>
           </View>
-        </View>
-      </Modal>
-    </View>
-  );
+        </Modal>
+      </View>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
@@ -350,18 +379,30 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
   textbox: {
-    backgroundColor: "gainsboro",
+    borderColor: "gainsboro",
+    borderWidth: 2,
     height: 40,
     width: "90%",
     padding: 10,
-    marginTop: 10,
-    marginBottom: 10,
+    marginTop: 7,
+    marginBottom: 7,
     borderRadius: 5,
   },
-  placecontainer: {
+  selectedplacecontainer: {
+    backgroundColor: "#FFD56D",
+    width: 250,
+    margin: 2,
+    padding: 3,
+    paddingLeft: 5,
+    borderRadius: 5,
+  },
+  unselectedplacecontainer: {
     backgroundColor: "gainsboro",
     width: 250,
     margin: 2,
+    padding: 3,
+    paddingLeft: 5,
+    borderRadius: 5,
   },
 });
 
